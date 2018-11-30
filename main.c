c480e34b07d0578e064806e500d24f4122187fc6
Alexandre PION <apion@student.42.fr>
2018-11-30 21:19:04 +0100

    improve main to loop on only one file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 14:00:24 by apion             #+#    #+#             */
-/*   Updated: 2018/11/29 17:31:51 by apion            ###   ########.fr       */
+/*   Updated: 2018/11/30 18:07:34 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int		main(int argc, char **argv)
 	int		fd1;
 	int		fd2;
 	char	*line;
+	int		ret;
 
 	if (argc == 1)
 		fd1 = 0;
@@ -25,16 +26,15 @@ int		main(int argc, char **argv)
 		fd1 = open(argv[1], O_RDONLY);
 	if (fd1 < 0)
 		ft_putstr_fd("An error occurred while opening the file1.\n", 2);
-	ft_putnbr(get_next_line(fd1, &line));
-	ft_putchar('\n');
-	if (line)
+	while ((ret = get_next_line(fd1, &line)) == 1)
+	{
+		ft_putnbr(ret);
+		ft_putchar('\n');
 		ft_putstr(line);
-	free(line);
-	ft_putnbr(get_next_line(fd1, &line));
-	ft_putchar('\n');
-	if (line)
-		ft_putstr(line);
-	
+		ft_putchar('\n');
+		free(line);
+	}
+	ft_putnbr(ret);
 	if (fd1 != 0)
 		close(fd1);
 	return (0);
